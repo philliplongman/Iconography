02837eae42d1ed622fc9f0bda7bd2634079fc5d6
Phillip Longman <himself@philliplongman.com>
2019-11-08 13:41:53 -0500

    Allow users to create Campaigns.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -4,4 +4,19 @@ class CampaignsController < ApplicationController
     @campaign = Campaign.find(params[:id])
   end
 
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = current_user.campaigns.create(campaign_params)
+    respond_with @campaign
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :description)
+  end
+
 end
