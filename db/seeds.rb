88d5628c920b0e5a17e958892e2959c86e97838f
Phillip Longman <himself@philliplongman.com>
2019-11-09 22:07:27 -0500

    Add Icons to seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,13 @@ user = User.find_or_create_by(email: "me@email.com") do |u|
   u.password_confirmation = "password1234"
 end
 
-user.campaigns.find_or_create_by(name: "13th Age Core Icons") do |c|
+campaign = user.campaigns.find_or_create_by(name: "13th Age Core Icons") do |c|
   c.description = <<~TEXT
     These are the standard icons as described in the 13th Age Core Rulebook.
   TEXT
 end
+
+campaign_data = File.read "lib/standard_icons.yml"
+standard_campaign = YAML.safe_load campaign_data, symbolize_names: true
+
+standard_campaign[:icons].each { |e| campaign.icons.find_or_create_by e }
