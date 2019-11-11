a51c688702783f91d58954f8b8c7b2f288687c5d
Phillip Longman <himself@philliplongman.com>
2019-11-10 21:46:21 -0500

    Add a button to create an Icon.

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -2,7 +2,7 @@ class IconsController < ApplicationController
 
   def index
     @campaign = Campaign.find(params[:campaign_id])
-    @icons = @campaign.icons.sorted
+    @age_icons = @campaign.icons.sorted
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :campaigns, only: %i[new create edit update] do
     resource :graph, only: %i[show]
-    resources :icons, only: %i[index show]
+    resources :icons, only: %i[index show new create]
   end
 
 end
