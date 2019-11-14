dac0c3d6d5ae6524de8adc72fe470fb1ec57aca8
Phillip Longman <himself@philliplongman.com>
2019-11-14 17:42:24 -0500

    Let users create Domains.
    But not yet associated Concerns.

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -5,4 +5,22 @@ class DomainsController < ApplicationController
     @domains = @campaign.domains.where.not(name: "Overall").includes(:icons)
   end
 
+  def new
+    @campaign = @campaigns.find(params[:campaign_id])
+    @domain = Domain.new
+  end
+
+  def create
+    @campaign = @campaigns.find(params[:campaign_id])
+    @domain = @campaign.domains.create(domain_params)
+
+    respond_with @domain, location: -> { [@campaign, :domains] }
+  end
+
+  private
+
+  def domain_params
+    params.require(:domain).permit(:name, :notes)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :campaigns, except: %i[index] do
     resource :graph, only: %i[show]
 
-    resources :domains, only: %i[index]
+    resources :domains, only: %i[index new create]
     resources :icons
   end
 
